8e074631ffa82e821cf50a66dd0e4f504e81d05b
Jordany <jordanyryan@gmail.com>
2020-02-04 14:44:06 -0500

    Updates

---
+++ precache-manifest.2656c3a716856422f55ae4c5cffadc9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc47de210370273fef2755cf2a1e2f7a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "49fa72b28639f8e69225",
+    "url": "/monsters-rolodex/static/css/main.e5b52bd0.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49fa72b28639f8e69225",
+    "url": "/monsters-rolodex/static/js/main.571f6aa3.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
